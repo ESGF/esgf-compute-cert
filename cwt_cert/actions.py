f1efba47455f158d7ded6f2ced84dd5599c40ffc
Jason Boutte <boutte.jason@gmail.com>
2018-10-08 23:40:05 +0000

    Adds status polling

--- cwt_cert/actions.py
+++ cwt_cert/actions.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import time
 
 import cwt
 
@@ -12,15 +13,32 @@ WPS_CAPABILITIES = 'wps_capabilities_action'
 WPS_EXECUTE = 'wps_execute_action'
 
 
+class ActionError(Exception):
+    pass
+
+
 def wps_execute_action(url, identifier, inputs, api_key, *args, **kwargs):
     client = cwt.WPSClient(url, verify=False, api_key=api_key)
 
-    process = [x for x in client.get_capabilities().processes if
-               re.match(identifier, x.identifier) is not None][0]
+    try:
+        process = [x for x in client.get_capabilities().processes if
+                   re.match(identifier, x.identifier) is not None][0]
+    except IndexError:
+        raise ActionError('Did not find a process matching {}'.format(
+            identifier))
 
     client.execute(process, inputs=inputs) 
 
-    return None
+    logger.info('%r', process.status)
+
+    while process.processing:
+        logger.info('%r', process.status)
+
+        time.sleep(1)
+
+    logger.info('%r', process.status)
+
+    return process.output
 
 
 def wps_capabilities_action(url, *args, **kwargs):
