5560d3f8320f4ac6e0e4d8564882c22f2eed797e
Jason Boutte <boutte.jason@gmail.com>
2019-01-09 23:49:30 -0800

    Adds initial security test

--- cwt_cert/test_server.py
+++ cwt_cert/test_server.py
@@ -1,9 +1,27 @@
 import cwt
+import pytest
+
+def test_security(context):
+    # Check SSL
+    # Check execute is protected
+    # ?
+
+    client = context.get_client()
+
+    process = client.processes('.*\.metrics')
+
+    assert len(process) > 0
+
+    process = process[0]
+
+    # Expect an error to be raised
+    with pytest.raises(cwt.WPSExceptionError):
+        client.execute(process)
 
 def test_official_operators(context):
     client = context.get_client()
 
-    expected = set(['aggregate', 'subset'])
+    expected = set(['aggregate', 'subset', 'metrics'])
 
     for method in expected:
         process = client.processes('.*\.{!s}'.format(method))
