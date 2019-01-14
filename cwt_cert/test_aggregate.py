aa5d57e94bc19fdb9f4ff6b1775fa3cb774b2766
Jason Boutte <boutte.jason@gmail.com>
2019-01-14 13:59:22 -0800

    Adds aggregate test

---
+++ cwt_cert/test_aggregate.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from process_base import ProcessBase
+
+class TestAggregate(ProcessBase):
+    identifier = 'aggregate'
+
+    performance = {
+        'variable': 'tas',
+        'files': [
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1870-01_cat_1879-12.nc',
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1880-01_cat_1889-12.nc',
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1890-01_cat_1899-12.nc',
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1900-01_cat_1909-12.nc',
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1910-01_cat_1919-12.nc',
+            'https://aims3.llnl.gov/thredds/dodsC/cmip3_data/data2/20c3m/atm/mo/tas/ncar_ccsm3_0/run1/tas_A1.20C3M_1.CCSM.atmm.1920-01_cat_1929-12.nc',
+        ],
+        'validations': {
+            'shape': (720, 128, 256),
+        }
+    }
