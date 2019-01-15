a38616becbae543b299941f9f9a4db332da66d4a
Jason Boutte <boutte.jason@gmail.com>
2019-01-14 23:32:24 -0800

    Adds setup file

--- cwt_cert/__init__.py
+++ cwt_cert/__init__.py
@@ -1,3 +1,9 @@
 import os
+import sys
 
 os.environ['UVCDAT_ANONYMOUS_LOG'] = 'no'
+
+import pytest
+
+def main():
+    pytest.main(['cwt_cert/', '-v'] + sys.argv)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cwt-certification',
+    description='Certification tool for Official ESGF Compute nodes',
+    packages=['cwt_cert'],
+    author='Jason Boutte',
+    author_email='boutte3@llnl.gov',
+    version='0.1.0',
+    url='https://github.com/ESGF/esgf-compute-cert',
+    entry_points={
+        'console_scripts': [
+            'cwt_cert = cwt_cert:main',
+        ]
+    },
+)
