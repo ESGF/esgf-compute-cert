851e787a30fc46cf94c45587136b2d4250ceb4ec
Jason Boutte <boutte.jason@gmail.com>
2019-01-14 22:29:25 -0800

    Adds json report file

--- cwt_cert/conftest.py
+++ cwt_cert/conftest.py
@@ -1,10 +1,8 @@
+import collections
+import json
+
 import cwt
 import pytest
-
-def pytest_addoption(parser):
-    parser.addoption('--host', required=True, help='')
-
-    parser.addoption('--token', required=True, help='')
 
 class Context(object):
     def __init__(self, host, token):
@@ -21,10 +19,67 @@ class Context(object):
 
         return client
 
-@pytest.fixture
-def context(request):
-    host = request.config.getoption('--host')
+class CWTCertificationReport(object):
+    def __init__(self, config):
+        self.config = config
 
-    token = request.config.getoption('--token')
+        self._context = None
+        self.tests = collections.OrderedDict()
 
-    return Context(host, token)
+    @pytest.fixture
+    def context(self, request):
+        if self._context is None:
+            host = request.config.getoption('--host')
+
+            token = request.config.getoption('--token')
+
+            self._context = Context(host, token)
+
+        return self._context
+
+    @pytest.hookimpl(hookwrapper=True)
+    def pytest_runtest_makereport(self, item, call):
+        outcome = yield
+
+        rep = outcome.get_result()
+
+        if rep.when == 'call':
+            self.tests[rep.nodeid] = {
+                'outcome': rep.outcome,
+                'longrepr': str(rep.longrepr),
+            }
+
+    def pytest_sessionfinish(self, session, exitstatus):
+        json_report_file = self.config.getoption('--json-report-file', None)
+
+        if json_report_file is not None:
+            report = {
+                'tests': self.tests,
+            }
+
+            with open(json_report_file, 'w') as outfile:
+                json.dump(report, outfile, indent=True)
+
+def pytest_addoption(parser):
+    group = parser.getgroup('cwt certification', 'cwt certification')
+
+    group.addoption('--host', required=True, help='target host to run tests on')
+
+    group.addoption('--token', required=True, help='token to be used for api access')
+
+    group.addoption('--json-report-file', help='path to store json report')
+
+def pytest_configure(config):
+    plugin = CWTCertificationReport(config)
+
+    config._cert_report = plugin
+
+    config.pluginmanager.register(plugin)
+
+def pytest_unconfigure(config):
+    plugin = getattr(config, '_cert_report', None)
+
+    if plugin is not None:
+        del config._cert_report
+
+        config.pluginmanager.unregister(plugin)
